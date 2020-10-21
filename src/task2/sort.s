96f5357bbfb6f3e280cbc0d0ea0f3781455e6eb9
Fertman Yakov <71883111+fertmanasa@users.noreply.github.com>
2020-10-21 11:53:45 +0300

    Update sort.s

--- src/task2/sort.s
+++ src/task2/sort.s
@@ -1,79 +1,63 @@
-	.section	__TEXT,__text,regular,pure_instructions
-	.build_version macos, 10, 15	sdk_version 10, 15, 6
-	.globl	_swap                   ## -- Begin function swap
-	.p2align	4, 0x90
-_swap:                                  ## @swap
+	.file	"main.c"
+	.text
+	.p2align 4,,15
+	.globl	swap
+	.type	swap, @function
+swap:
+.LFB0:
 	.cfi_startproc
-## %bb.0:
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register %rbp
 	movl	(%rsi), %eax
 	addl	(%rdi), %eax
 	movl	%eax, (%rdi)
 	subl	(%rsi), %eax
 	movl	%eax, (%rsi)
 	subl	%eax, (%rdi)
-	popq	%rbp
-	retq
+	ret
 	.cfi_endproc
-                                        ## -- End function
-	.globl	_bubblesort             ## -- Begin function bubblesort
-	.p2align	4, 0x90
-_bubblesort:                            ## @bubblesort
+.LFE0:
+	.size	swap, .-swap
+	.p2align 4,,15
+	.globl	bubblesort
+	.type	bubblesort, @function
+bubblesort:
+.LFB1:
 	.cfi_startproc
-## %bb.0:
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register %rbp
 	testl	%esi, %esi
-	jle	LBB1_7
-## %bb.1:
-	movl	%esi, %r8d
-	leaq	-1(%r8), %r9
-	xorl	%r10d, %r10d
-	movq	%rdi, %rsi
-	jmp	LBB1_2
-	.p2align	4, 0x90
-LBB1_6:                                 ##   in Loop: Header=BB1_2 Depth=1
-	incq	%r10
-	addq	$4, %rsi
-	decq	%r9
-	cmpq	%r8, %r10
-	je	LBB1_7
-LBB1_2:                                 ## =>This Loop Header: Depth=1
-                                        ##     Child Loop BB1_5 Depth 2
-	movl	(%rdi,%r10,4), %ecx
-	xorl	%eax, %eax
-	movl	%ecx, %edx
-	cmpl	%ecx, %edx
-	jge	LBB1_5
-	.p2align	4, 0x90
-LBB1_4:                                 ##   in Loop: Header=BB1_2 Depth=1
-	addl	%edx, %ecx
-	movl	%ecx, (%rdi,%r10,4)
-	subl	(%rsi,%rax,4), %ecx
-	movl	%ecx, (%rsi,%rax,4)
-	movl	(%rdi,%r10,4), %edx
-	subl	%ecx, %edx
-	movl	%edx, (%rdi,%r10,4)
-LBB1_5:                                 ##   Parent Loop BB1_2 Depth=1
-                                        ## =>  This Inner Loop Header: Depth=2
-	cmpq	%rax, %r9
-	je	LBB1_6
-## %bb.8:                               ##   in Loop: Header=BB1_5 Depth=2
-	movl	4(%rsi,%rax,4), %ecx
-	incq	%rax
-	cmpl	%ecx, %edx
-	jl	LBB1_4
-	jmp	LBB1_5
-LBB1_7:
-	popq	%rbp
-	retq
+	jle	.L3
+	leal	-1(%rsi), %r10d
+	movq	%rdi, %r8
+	xorl	%r9d, %r9d
+	addq	$1, %r10
+	.p2align 4,,10
+	.p2align 3
+.L7:
+	movl	(%r8), %ecx
+	movq	%r9, %rdx
+	.p2align 4,,10
+	.p2align 3
+.L6:
+	movl	(%rdi,%rdx,4), %eax
+	cmpl	%ecx, %eax
+	jle	.L5
+	addl	%ecx, %eax
+	movl	%eax, (%r8)
+	subl	(%rdi,%rdx,4), %eax
+	movl	%eax, (%rdi,%rdx,4)
+	movl	(%r8), %ecx
+	subl	%eax, %ecx
+	movl	%ecx, (%r8)
+.L5:
+	addq	$1, %rdx
+	cmpl	%edx, %esi
+	jg	.L6
+	addq	$1, %r9
+	addq	$4, %r8
+	cmpq	%r9, %r10
+	jne	.L7
+.L3:
+	rep ret
 	.cfi_endproc
-                                        ## -- End function
-.subsections_via_symbols
+.LFE1:
+	.size	bubblesort, .-bubblesort
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
